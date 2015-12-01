17040d3301acf01c7c8fe47eea4c1a13a6f82853
Sean Holcomb <seanholcomb@gmail.com>
2015-11-30 16:06:25 -0800

    feat: add basic movie class

---
+++ media.py
@@ -0,0 +1,8 @@
+
+
+class Movie:
+
+    def __init__(self, title, image_url, trailer_url):
+        self.title = title
+        self.poster_image_url = image_url
+        self.trailer_youtube_id = trailer_url
