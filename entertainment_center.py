9091fd9b46263394e814db0b4ca7c81e0038d90b
Sean Holcomb <seanholcomb@gmail.com>
2015-11-30 19:27:51 -0800

    feat: add release date

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,13 +4,13 @@ from media import Movie
 
 def make_page():
     """create move objects and display them in a webbrowser"""
-    mad_max = Movie("Mad Max: Fury Road",
+    mad_max = Movie("Mad Max: Fury Road", "May 15, 2015",
                     "http://ia.media-imdb.com/images/M/MV5BMTUyMTE0ODcxNF5BMl5BanBnXkFtZTgwODE4NDQzNTE@._V1_SY317_CR2,0,214,317_AL_.jpg",
                     "https://www.youtube.com/watch?v=hEJnMQG9ev8")
-    bad_boys = Movie("Bad Boys",
+    bad_boys = Movie("Bad Boys", "March 25, 1983",
                      "http://www.gstatic.com/tv/thumb/movieposters/6963/p6963_p_v7_aa.jpg",
                      "https://www.youtube.com/watch?v=RRILgwNJNkI")
-    enter_the_void = Movie("Enter the Void",
+    enter_the_void = Movie("Enter the Void", "September 24, 2010",
                            "https://upload.wikimedia.org/wikipedia/en/3/39/Enter-the-void-poster.png",
                            "https://www.youtube.com/watch?v=bKRxDP--e-Y")
     fresh_tomatoes.open_movies_page([mad_max, bad_boys, enter_the_void])
--- media.py
+++ media.py
@@ -1,10 +1,12 @@
 
 class Movie:
     """Movie class to be displayed in tiles on webpage """
-    def __init__(self, title, image_url, trailer_url):
+    def __init__(self, title, release_date, image_url, trailer_url):
         """Constructor for move class. Takes the movie's title along with url's for its poster and trailer on youtube"""
         self.title = title
+        self.release_date = release_date
         self.poster_image_url = image_url
         self.trailer_youtube_url = trailer_url
+
 
 
