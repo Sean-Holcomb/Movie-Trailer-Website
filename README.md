b4806081d39462a629cd26631c6357d1904a4e86
Sean Holcomb <seanholcomb@gmail.com>
2015-11-30 19:11:11 -0800

    doc:add README and comments

---
+++ README.md
@@ -0,0 +1,8 @@
+#Movie Website
+Project for Udacity Fullstack Nanodegree. 
+Open a basic movie trailer website. 
+Click on movie poster to play trailer.
+##Use
+1. Install Python
+2. Navigate to directory in terminal
+3. run command `python entertainment_center.py`
--- media.py
+++ media.py
@@ -1,21 +1,10 @@
-import fresh_tomatoes
 
 class Movie:
-
+    """Movie class to be displayed in tiles on webpage """
     def __init__(self, title, image_url, trailer_url):
+        """Constructor for move class. Takes the movie's title along with url's for its poster and trailer on youtube"""
         self.title = title
         self.poster_image_url = image_url
-        self.trailer_youtube_id = trailer_url
+        self.trailer_youtube_url = trailer_url
 
 
-def make_page():
-    mad_max = Movie("Mad Max: Fury Road",
-                    "http://ia.media-imdb.com/images/M/MV5BMTUyMTE0ODcxNF5BMl5BanBnXkFtZTgwODE4NDQzNTE@._V1_SY317_CR2,0,214,317_AL_.jpg",
-                    "https://www.youtube.com/watch?v=hEJnMQG9ev8")
-    bad_boys = Movie("Bad Boys",
-                     "http://www.gstatic.com/tv/thumb/movieposters/6963/p6963_p_v7_aa.jpg",
-                     "https://www.youtube.com/watch?v=RRILgwNJNkI")
-    enter_the_void = Movie("Enter the Void",
-                           "https://upload.wikimedia.org/wikipedia/en/3/39/Enter-the-void-poster.png",
-                           "https://www.youtube.com/watch?v=bKRxDP--e-Y")
-    fresh_tomatoes.open_movies_page([mad_max, bad_boys, enter_the_void])
